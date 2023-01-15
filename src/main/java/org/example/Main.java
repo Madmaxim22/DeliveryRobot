4ec084745620aea1616438876990ed2d6b756114
Maksim <madmaxim22@gmail.com>
2023-01-15 17:43:58 +0300

    add maxEntryRunnable and change Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,29 +8,22 @@ import static org.example.Main.sizeToFreq;
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        maxEntryRunnable maxEntryRunnable = new maxEntryRunnable();
+        Thread thread = new Thread(maxEntryRunnable);
+        thread.start();
 
-        for (int i = 0; i < 1000; i++) {
-            myRunnable mr = new myRunnable();
-            new Thread(mr).start();
+        for (int i = 0; i < 50; i++) {
+            numberOfTurnsToTheRightRunnable number = new numberOfTurnsToTheRightRunnable();
+            Thread thread1 = new Thread(number);
+            thread1.start();
+            thread1.join();
         }
-
-        Map.Entry<Integer, Integer> maxEntry = sizeToFreq
-                .entrySet()
-                .stream()
-                .max(Map.Entry.comparingByValue())
-                .orElse(null);
-        sizeToFreq.remove(maxEntry.getKey());
-        System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n", maxEntry.getKey(), maxEntry.getValue());
-        System.out.println("другие размеры:");
-        sizeToFreq.entrySet()
-                .stream()
-                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
-                .forEach(s -> System.out.printf("- %d (%d раз)\n", s.getKey(), s.getValue()));
+        thread.interrupt();
     }
 }
 
-class myRunnable implements Runnable {
+class numberOfTurnsToTheRightRunnable implements Runnable {
 
     @Override
     public void run() {
@@ -40,6 +33,26 @@ class myRunnable implements Runnable {
                 .count());
         synchronized (sizeToFreq) {
             sizeToFreq.merge(turnRight, 1, Integer::sum);
+            sizeToFreq.notify();
+        }
+    }
+}
+
+class maxEntryRunnable implements Runnable {
+    @Override
+    public void run() {
+        while (!Thread.interrupted()) {
+            synchronized (sizeToFreq) {
+                try {
+                    sizeToFreq.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Map.Entry<Integer, Integer> maxEntry = sizeToFreq.entrySet().stream()
+                        .max(Map.Entry.comparingByValue())
+                        .orElse(null);
+                System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n", maxEntry.getKey(), maxEntry.getValue());
+            }
         }
     }
 }
